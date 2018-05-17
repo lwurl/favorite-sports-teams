7d5869a38f3eeac90cd9a6393f6982b03cc5ad59
Luke Wurl <lwurl@nd.edu>
2018-05-17 15:14:23 -0400

    Add flicks to an array

--- index2.js
+++ index2.js
@@ -1,34 +1,36 @@
 const app = {
     init(selectors) {
-      this.max = 0
-      this.list = document.querySelector(selectors.listSelector)
-  
-      document
-        .querySelector(selectors.formSelector)
-        .addEventListener('submit', ev => {
-          ev.preventDefault()
-          this.handleSubmit(ev)
-        })
+        this.flicks = []
+        this.max = 0
+        this.list = document.querySelector(selectors.listSelector)
+
+        document
+            .querySelector(selectors.formSelector)
+            .addEventListener('submit', ev => {
+                ev.preventDefault()
+                this.handleSubmit(ev)
+            })
     },
 
-    renderListItem(flick){
+    renderListItem(flick) {
         const item = document.createElement('li')
         item.textContent = flick.name
         return item
     },
-  
+
     handleSubmit(ev) {
-      const f = ev.target
-      const flick = {
-        id: ++this.max,
-        name: f.flickName.value,
-      }
-      const item = this.renderListItem(flick)
-      this.list.appendChild(item)
-      f.reset()
+        const f = ev.target
+        const flick = {
+            id: ++this.max,
+            name: f.flickName.value,
+        }
+        this.flicks.push(flick)
+        const item = this.renderListItem(flick)
+        this.list.appendChild(item)
+        f.reset()
     },
 }
-  
+
 app.init({
     formSelector: '#flickForm',
     listSelector: '#flickList',
