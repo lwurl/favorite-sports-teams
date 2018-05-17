16f99cb175c38cac047dc9705a58c8ed478a20a3
Luke Wurl <lwurl@nd.edu>
2018-05-17 15:10:06 -0400

    Switch to Tatum Tots

--- index2.js
+++ index2.js
@@ -1,22 +1,35 @@
 const app = {
-    init: function(formSelector){
-        this.max = 0
-        document
-            .querySelector(formSelector)
-            .addEventListener('submit', (ev) => {
-                ev.preventDefault()
-                this.handleSubmit(ev)
-            })
+    init: function(selectors) {
+      this.max = 0
+      this.list = document.querySelector(selectors.listSelector)
+  
+      document
+        .querySelector(selectors.formSelector)
+        .addEventListener('submit', ev => {
+          ev.preventDefault()
+          this.handleSubmit(ev)
+        })
     },
-    handleSubmit : function(ev){
-        const f = ev.target
-        const team = {
-            id : ++this.max,
-            name : f.teamName.value,
-        }
-        console.log(team)
-        f.reset()
+
+    renderListItem: function(flick){
+        const item = document.createElement('li')
+        item.textContent = flick.name
+        return item
+    },
+  
+    handleSubmit: function(ev) {
+      const f = ev.target
+      const flick = {
+        id: ++this.max,
+        name: f.flickName.value,
+      }
+      const item = this.renderListItem(flick)
+      this.list.appendChild(item)
+      f.reset()
     },
 }
-
-app.init('#teamForm')
+  
+app.init({
+    formSelector: '#flickForm',
+    listSelector: '#flickList',
+})
